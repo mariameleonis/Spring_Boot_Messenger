f6992156a24fb4c34502993a5253b4146917f5f9
Mariya_Russakova <Mariya_Russakova@epam.com>
2023-01-15 14:55:48 +0600

    test generateMessage_shouldNeverReturnNull() failed

---
+++ src/test/java/com/epam/ld/module2/testing/TemplateUtilsTest.java
@@ -0,0 +1,20 @@
+package com.epam.ld.module2.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.epam.ld.module2.testing.model.MessageTemplate;
+import java.util.Map;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+class TemplateUtilsTest {
+
+  @Test
+  void generateMessage_shouldNeverReturnNull() {
+    val template = MessageTemplate.builder().build();
+    val values = Map.of(
+        "name", "Mariya");
+    val result = TemplateUtils.generateMessage(template, values);
+    assertNotNull(result);
+  }
+}
