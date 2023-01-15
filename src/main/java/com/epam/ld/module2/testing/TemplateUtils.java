9ad955dff532b4f57fb20f7aee567dd1e841a8f6
Mariya_Russakova <Mariya_Russakova@epam.com>
2023-01-15 15:03:29 +0600

    test generateMessage_messageSubjectShouldNeverBeNull() passed

--- src/main/java/com/epam/ld/module2/testing/TemplateUtils.java
+++ src/main/java/com/epam/ld/module2/testing/TemplateUtils.java
@@ -11,7 +11,7 @@ public class TemplateUtils {
   }
 
   public static Message generateMessage(MessageTemplate template, Map<String, String> templateValues) {
-    return new Message(null, null);
+    return new Message("", null);
   }
 
   public static Map<String, String> readTemplateValues() {
