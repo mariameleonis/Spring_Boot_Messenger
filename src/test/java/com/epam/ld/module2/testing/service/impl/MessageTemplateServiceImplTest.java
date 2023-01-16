08cb0d3a2aa72d0cf6c745a87193d51f9310c48e
Mariya_Russakova <Mariya_Russakova@epam.com>
2023-01-16 23:20:28 +0600

    delete findAll_shouldNotReturnEmptyList() (outdated)

--- src/test/java/com/epam/ld/module2/testing/service/impl/MessageTemplateServiceImplTest.java
+++ src/test/java/com/epam/ld/module2/testing/service/impl/MessageTemplateServiceImplTest.java
@@ -1,9 +1,13 @@
 package com.epam.ld.module2.testing.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.epam.ld.module2.testing.model.MessageTemplate;
 import com.epam.ld.module2.testing.repository.MessageTemplateRepository;
+import java.util.Collections;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,14 +25,16 @@ class MessageTemplateServiceImplTest {
   private MessageTemplateServiceImpl service;
 
   @Test
-  void findAll_shouldNotReturnEmptyList() {
-    val result = service.findAll();
-    assertFalse(result.isEmpty());
-  }
-
-  @Test
   void findAll_shouldCallRepositoryFindAll() {
     service.findAll();
     verify(repository).findAll();
+  }
+
+  @Test
+  void findAll_shouldReturnAListOfFoundMessageTemplates() {
+    val messageTemplates = Collections.singletonList(MessageTemplate.builder().build());
+    when(repository.findAll()).thenReturn(messageTemplates);
+    val result = service.findAll();
+    assertEquals(messageTemplates, result);
   }
 }
